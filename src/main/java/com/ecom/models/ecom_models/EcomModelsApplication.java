64b0ab83591075d48a32f79862f706aaae0570a8
Nikhil Madaan <nikhilmadaan29@gmail.com>
2024-10-07 22:08:15 +0530

    Initial commit for models

---
+++ src/main/java/com/ecom/models/ecom_models/EcomModelsApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.models.ecom_models;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomModelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomModelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecom/models/ecom_models/EcomModelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.models.ecom_models;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomModelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
